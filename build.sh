8651bc25b301f3a54e9e214b45329fb26167a940
Yang Zhang <yangzhan23@mails.tsinghua.edu.cn>
2025-02-06 17:22:05 +0800

    build: build umicollapse here

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-mkdir bin
+mkdir -p bin
 javac --release 11 -cp "lib/htsjdk-4.1.3-imp.jar:lib/snappy-java-1.1.7.3.jar" -d bin src/umicollapse/*/*.java src/test/*.java
 cd bin
-jar -c -m ../Manifest.txt -f ../../umicollapse-new.jar umicollapse/*/*.class test/*.class
+jar -c -m ../Manifest.txt -f ../umicollapse.jar umicollapse/*/*.class test/*.class
 
 
 echo "Done!"
